63ec9bf0d22c965d691716a699022afb4527942b
Matt Corwin <mcorwin@aais.com>
2019-04-22 10:55:48 -0500

    initial commit, basic app complete

---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from "redux";
+
+const songsReducer = () => {
+  return [
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "Smells Like Teen Spirit", duration: "3:05" },
+    { title: "Macarena", duration: "2:45" },
+    { title: "C.R.E.A.M.", duration: "5:05" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
